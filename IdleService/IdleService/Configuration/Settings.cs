f5ea6f15d8289b474afa787a37895bd648d27b69
LostSoulFly <lostsoulfly@users.noreply.github.com>
2017-12-04 19:18:03 -0500

    Fixed loading from JSON
    Had to use JsonProperty because of the private setters, without those it would have worked.

--- IdleService/IdleService/Configuration/Settings.cs
+++ IdleService/IdleService/Configuration/Settings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,21 +10,35 @@ namespace IdleService
     class Settings
     {
         //Settings loaded from config file
+        [JsonProperty]
         public bool enableDebug { get; private set; }
+        [JsonProperty]
         public static bool enableLogging { get; private set; }
         //public static string logFilePath { get; private set; } Not sure how I want to do this yet.
 
+        [JsonProperty]
         public bool stealthMode { get; private set; }
+        [JsonProperty]
         public bool preventSleep { get; private set; }
+        [JsonProperty]
         public bool monitorCpuTemp { get; private set; }
+        [JsonProperty]
         public int maxCpuTemp { get; private set; }
+        [JsonProperty]
         public int cpuUsageThresholdWhileNotIdle { get; private set; }
+        [JsonProperty]
         public bool monitorGpuTemp { get; private set; }
+        [JsonProperty]
         public int maxGpuTemp { get; private set; }
+        [JsonProperty]
         public bool mineIfBatteryNotFull { get; private set; }
+        [JsonProperty]
         public bool verifyNetworkConnectivity { get; private set; }
+        [JsonProperty]
         public string urlToCheckForNetwork { get; private set; }
+        [JsonProperty]
         public int minutesUntilIdle { get; private set; }
+        [JsonProperty]
         public int resumePausedMiningAfterMinutes { get; private set; }
 
         public void SetupDefaultConfig()
