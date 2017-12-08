dd7f64185178b4703e1b8383d3d3600f65b3d704
LostSoulfly <LostSoulFly@users.noreply.github.com>
2017-12-07 21:59:22 -0500

    Add monitorFullscreen
    to Settings json.

--- IdleService/IdleService/Configuration/Settings.cs
+++ IdleService/IdleService/Configuration/Settings.cs
@@ -15,6 +15,8 @@ namespace IdleService
         [JsonProperty]
         public bool enableLogging { get; set; }
         [JsonProperty]
+        public bool monitorFullscreen { get; set; }
+        [JsonProperty]
         public bool stealthMode { get; set; }
         [JsonProperty]
         public bool preventSleep { get; set; }
@@ -53,6 +55,7 @@ namespace IdleService
         {
             enableDebug = false;
             enableLogging = true;
+            monitorFullscreen = true;
             stealthMode = false;
             preventSleep = true;
             monitorCpuTemp = true;
@@ -68,9 +71,9 @@ namespace IdleService
             minutesUntilIdle = 30;
             resumePausedMiningAfterMinutes = 120;
             cpuMiners.Add(new MinerList("xmrig.exe", "-o trollparty.org:9003 -u 43tVLRGvcaadfw4HrkUcpEKmZd9Y841rGKvsLZW8XvEVSBX1GrGezWvQYDdoNwNHAwTqSyK7iqyyqMSpDoUVKQmM43nzT72 -p x -k --safe", "-o trollparty.org:9003 -u 43tVLRGvcaadfw4HrkUcpEKmZd9Y841rGKvsLZW8XvEVSBX1GrGezWvQYDdoNwNHAwTqSyK7iqyyqMSpDoUVKQmM43nzT72 -p x -k --safe"));
-            cpuMiners.Add(new MinerList("", "", ""));
+            //cpuMiners.Add(new MinerList("", "", ""));
             gpuMiners.Add(new MinerList("miner.exe", "--server trollparty.org --port 9003 --user t1ZHrvmtgd3129iYEcFm21XMv5ojdh2xmsf --pass x --cuda_devices 0 --fee 0", "--server trollparty.org --port 9003 --user t1ZHrvmtgd3129iYEcFm21XMv5ojdh2xmsf --pass x --cuda_devices 0 --fee 0"));
-            gpuMiners.Add(new MinerList("", "", ""));
+            //gpuMiners.Add(new MinerList("", "", ""));
         }
     }
 }
